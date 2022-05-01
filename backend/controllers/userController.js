2d92be504d53f0b7c729d7deedbc6db01f91acc2
Siddharth Raj <siddharthraj34@yahoo.com>
2022-05-02 03:05:37 +0530

    Login andJwt created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,85 @@
-const asyncHandler = require('express-async-handler')
-const bcrypt =require('bcryptjs')
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require ('jsonwebtoken')
 
-const User =require('../models/userModels')
+const User = require("../models/userModels");
 
 //@desc Register a new user
 //@route /api/users
 //@access Public
-const registerUser = asyncHandler( async (req,res) => {
-    const {name,email,password} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-    //validation
-    if(!name || !email || !password){
-         res.status(400)
-        throw new Error('Please include all all fields')
-    }
-    
-    res.send('Register route')
-})
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all all fields");
+  }
+
+  //Find user if aleready exist
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
+});
 
 //@desc Login a new users
 //@route /api/users.login
 //@access Public
-const loginUser =asyncHandler( async (req,res) => {
-    res.send('login route')
-})
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, login, password } = req.body;
 
-module.exports={
-    registerUser,
-    loginUser
+  const user = await User.findOne({ email });
+
+  //check user and password is match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  }
+   else {
+       res.status(401)
+       throw new Error('Invalid Crdentaials')
+  }
+});
+
+//Genrate token
+
+const generateToken = (id) =>{
+    return jwt.sign({id}, process.env.JWT_SECRET,{
+        expiresIn: '30d',
+    })
 }
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
